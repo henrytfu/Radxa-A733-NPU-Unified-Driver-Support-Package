732609b5b853dc2e9790040a1abc755ac3a41120
henry <henry@anonymous>
2026-05-11 18:15:37 +0100

    Add a script for producing a debian package (WIP)

---
+++ make_debian.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Make sure the repository is clean before we start. We don't want to mess with any uncommitted changes.
+git restore ./
+git clean -fdx ./
+
+# Apply the kernel 6.6 patch
+git apply --check galcore_6.6_kernel_api_drift.patch
+
+# We are ready to start building the Debian package
+kdir="/lib/modules/$(uname -r)/build"
+srcdir = "$(pwd)/aw_nna_galcore"
+
+make -C "$kdir" M="$srcdir" modules
