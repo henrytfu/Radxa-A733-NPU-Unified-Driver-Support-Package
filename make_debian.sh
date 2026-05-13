3ca8ae3215bddbfece837dffcfe3538fd9cf1d49
henry <henry@anonymous>
2026-05-13 22:03:47 +0100

    Update make debian.sh

--- make_debian.sh
+++ make_debian.sh
@@ -15,4 +15,22 @@ srcdir="$(pwd)/aw_nna_galcore"
 
 make -C "$kdir" M="$srcdir" modules
 
-mkdir -p pkg/
+# 2. Setup Staging Environment
+mkdir -p "pkg/lib/modules/$(uname -r)/extra"
+mkdir -p "pkg/etc/udev/rules.d"
+mkdir -p "pkg/etc/modprobe.d"
+mkdir -p "pkg/DEBIAN"
+
+# 3. COPY existing config files instead of echoing
+cp "$srcdir/galcore.ko" "pkg/lib/modules/$(uname -r)/extra/"
+cp "debian-config/99-galcore.rules" "pkg/etc/udev/rules.d/"
+cp "debian-config/postinst" "pkg/DEBIAN/"
+
+# Ensure the postinst is executable (Mandatory for dpkg)
+chmod 755 "pkg/DEBIAN/postinst"
+
+# 4. Generate the Control file
+cp "debian-config/control" "pkg/DEBIAN/control"
+
+# 5. Build the final .deb
+dpkg-deb --build "pkg" "galcore-unified-driver-a733.deb"
